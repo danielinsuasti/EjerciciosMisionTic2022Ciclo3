b9e5108ab5f32ce0ce22e7415e995268930341b8
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2021-09-23 17:51:56 -0500

    final CLASE 11, Finalizacion ejercicio consumo APi OMDB con bootstrap

--- GRUPO51/CLASE11/Ejercicio6.js
+++ GRUPO51/CLASE11/Ejercicio6.js
@@ -21,9 +21,7 @@ let obtenerTexto = () => {
             consultaApi(textoBuscar.value)
         }else{
             alert("Realize una busqueda adecuada")
-        }
-
-        
+        }      
 
 
     })
@@ -33,9 +31,49 @@ obtenerTexto();
 
 consultaApi = async data =>{
 
+    let urlApi = `http://www.omdbapi.com/?apikey=23daade9&t=${data}&plot=full&type=movie`
+
     try {
+        let responseText = await fetch(urlApi, {method: 'GET'});
+        console.log(responseText);
+        let responseJson = await responseText.json();
+        console.log(responseJson);
+        mostrarPelicula(responseJson);
         
     } catch (error) {
         
     }
+
+}
+
+
+
+const mostrarPelicula = (matrix) =>{
+
+    // console.log(matrix)
+
+    //Mostrar elementos en el DOM
+   //2. Crear los elementos en html y acceder a sus atributos para modificar el html
+    let tituloPelicula = document.getElementById("tituloPelicula");
+    let imagenPelicula = document.getElementById("imagenPelicula");
+    let plotPelicula = document.getElementById("plotPelicula");
+    let actoresPelicula = document.getElementById("actoresPelicula");
+    let fechaPelicula = document.getElementById("fechaPelicula");
+    let enlacePelicula = document.getElementById("enlacePelicula");
+
+
+    tituloPelicula.innerText = matrix.Title;
+    
+    imagenPelicula.src = matrix.Poster;
+    imagenPelicula.alt = "Poster Pelicula The Matrix";
+    plotPelicula.innerText = matrix.Plot;
+    
+    actoresPelicula.innerText = matrix.Actors;
+    
+    fechaPelicula.innerText = matrix.Year;
+    
+    enlacePelicula.innerText = "Sitio Oficial IMDB";
+    enlacePelicula.href = `https://www.imdb.com/title/${matrix.imdbID}`;
+    enlacePelicula.target = "_blank";
+
 }
