1d5c5e2e6e1771894ce244499d94bb220d00f854
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2021-09-27 20:04:40 -0500

    Clase 13, Grupo 51, inicio ejercicio Gestion Tareas Vue

---
+++ GRUPO51/CLASE13/4_VueTareas/app.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: "#app",
+    data :{ 
+        titulo :  "Administrador de Tareas con Vue, Grupo 51",
+        nuevaTarea : "",
+        tareas: []     
+    },
+    methods :{
+        agregarTareas(){
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            this.nuevaTarea = "";
+
+        }
+
+
+    },    
+    created(){
+
+    }
+
+})
