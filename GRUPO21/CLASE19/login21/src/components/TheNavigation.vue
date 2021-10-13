dd8dee4bee078898c5cd59c0517badb47dbb3e59
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2021-10-13 17:56:35 -0500

    Grupo 21, inicio Clase 19 Zona administracion Vuetify

---
+++ GRUPO21/CLASE19/login21/src/components/TheNavigation.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="About">About</router-link>
+      <router-link to="Login" v-if="!isLogin">Login</router-link>
+      <router-link to="Admin" v-else>Admin</router-link>      
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'TheNavigation',
+    computed : {
+      isLogin(){
+        return localStorage.getItem('token') //Revisar si existe un token en el localstorage, si eso pasa es porque estamos logueados
+      }
+    }
+}
+</script>
+
+
+<style scoped>
+
+#nav {
+    display: flex;
+    padding: 30px;
+    align-items: center;
+    justify-content: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 40px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+</style>
